31ce6270721e351d086ba05e1f2ce95fde3b742d
Thomlock <thomas.buendia@outlook.fr>
2026-06-20 23:17:48 +0200

    Delete files directory

--- files/obj/Release/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace PostbirdTAS
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "PostbirdTAS";
-        public const string PLUGIN_NAME = "PostbirdTAS";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
